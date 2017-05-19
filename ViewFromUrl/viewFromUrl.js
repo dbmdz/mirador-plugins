227de61c022cd6f876a405b5a4b7b02ef4e7d04f
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2017-05-19 14:37:02 +0200

    Add functionality for updating the url when the preultimate slot is closed

--- ViewFromUrl/viewFromUrl.js
+++ ViewFromUrl/viewFromUrl.js
@@ -13,12 +13,12 @@ var UpdateUrlFromView = {
           loadedManifest: params.manifest,
           canvasID: params.canvas
         };
-        this.state['currentConfig'].windowObjects = [
-          $.extend(true, {}, this.state['currentConfig'].windowObjects[0], windowObj)
+        this.state.currentConfig.windowObjects = [
+          $.extend(true, {}, this.state.currentConfig.windowObjects[0], windowObj)
         ];
       }
-      this.eventEmitter.subscribe('slotsUpdated', this_.onSlotsUpdated);
-      this.eventEmitter.subscribe('windowUpdated', this_.onWindowUpdated);
+      this.eventEmitter.subscribe('slotsUpdated', this_.onSlotsUpdated.bind(this_));
+      this.eventEmitter.subscribe('windowUpdated', this_.onWindowUpdated.bind(this_));
       origFunc.apply(this);
     }
   },
@@ -37,8 +37,8 @@ var UpdateUrlFromView = {
     this.updateUrl = data.slots.length === 1;
     if (!this.updateUrl) {
       window.history.replaceState({}, null, "?");
-    } else {
-      console.log('Activated URL update');
+    } else if (data.slots[0].window !== null) {
+      window.history.replaceState({}, null, "?" + this.constructSearchParams(data.slots[0].window));
     }
   },
 
@@ -46,13 +46,21 @@ var UpdateUrlFromView = {
     if (!this.updateUrl || !data.viewType) {
       return;
     }
+    window.history.replaceState({}, null, "?" + this.constructSearchParams(data));
+  },
+
+  constructSearchParams: function(data) {
     var newParams = new URLSearchParams();
     newParams.set('view', data.viewType);
-    newParams.set('manifest', data.loadedManifest);
+    if (data.loadedManifest) {
+      newParams.set('manifest', data.loadedManifest);
+    } else {
+      newParams.set('manifest', data.manifest.uri);
+    }
     if (data.canvasID) {
       newParams.set('canvas', data.canvasID);
     }
-    window.history.replaceState({}, null, "?" + newParams.toString());
+    return newParams.toString();
   }
 }
 

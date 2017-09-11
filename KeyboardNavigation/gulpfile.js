59dfda8e51a728c48bb73b07c9c8a7629732379d
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2017-09-11 12:58:33 +0200

    Add minifying of sources to the last plugin

---
+++ KeyboardNavigation/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var linter = require('gulp-jshint');
+var minifyJS = require('gulp-minify');
+var rename = require('gulp-rename');
+
+gulp.task('clean', function(){
+  return gulp.src(
+    '*.min.js', {read: false}
+  ).pipe(clean());
+});
+
+gulp.task('lint', function(){
+  return gulp.src('*.js').pipe(
+    linter()
+  ).pipe(
+    linter.reporter('default')
+  );
+});
+
+gulp.task('minify-js', ['clean'], function(){
+  return gulp.src(['*.js', '!gulpfile.js']).pipe(
+    minifyJS({
+      exclude: ['dist'],
+      ext:{
+        min:'.min.js'
+      },
+      noSource: true
+    })
+  ).pipe(
+    gulp.dest('.')
+  );
+});
+
+gulp.task('minify', ['clean', 'minify-js']);

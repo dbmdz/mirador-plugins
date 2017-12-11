d76633ebdb26cf7c5229b968f1c2d2114bec0c7f
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2017-12-11 13:18:17 +0100

    PhysicalRuler: Add polyfill for Array.prototype.find, should fix IE11

--- PhysicalRuler/physicalRuler.js
+++ PhysicalRuler/physicalRuler.js
@@ -8,6 +8,33 @@
  * - Evaluate using SVG for drawing the rulers
  */
 
+// Array.prototype.find polyfill
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+      var o = Object(this);
+      var len = o.length >>> 0;
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+      var thisArg = arguments[1];
+      var k = 0;
+      while (k < len) {
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        k++;
+      }
+      return undefined;
+    }
+  });
+}
+
 // Hook up OpenSeadragon plugin
 (function(osd) {
   // ChildNode.remove() polyfill

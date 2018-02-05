09238a9bf758f4ac0231642fd5c84c41eb610f17
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-02-05 15:54:35 +0100

    Fix bug with display in fullscreen

--- ImageCropper/imageCropper.js
+++ ImageCropper/imageCropper.js
@@ -24,7 +24,7 @@ var ImageCropper = {
       'pixel': 'Pixel',
       'preview': 'Vorschau',
       'preview-image-error': 'Die ausgewählten Parameter werden für diese Ressource leider nicht unterstützt!',
-      'preview-image-link': '<a href="#" id="preview-image-link" target="_blank">In oben ausgewählter Größe anzeigen</a>',
+      'preview-image-link': 'In oben ausgewählter Größe anzeigen',
       'quality': 'Qualität',
       'region-link': 'Link zum ausgewählten Ausschnitt',
       'rotation': 'Rotation',
@@ -43,7 +43,7 @@ var ImageCropper = {
       'pixel': 'pixels',
       'preview': 'Preview',
       'preview-image-error': 'The selected parameters are not supported for this resource!',
-      'preview-image-link': '<a href="#" id="preview-image-link" target="_blank">Show in size selected above</a>',
+      'preview-image-link': 'Show in size selected above',
       'quality': 'Quality',
       'region-link': 'Link to the selected region',
       'rotation': 'Rotation',
@@ -84,7 +84,7 @@ var ImageCropper = {
 
   /* the template for the modal containing the image url for the selection */
   modalTemplate: Mirador.Handlebars.compile([
-    '<div id="image-cropper-modal" class="modal fade" tabindex="-1" role="dialog">',
+    '<div id="image-cropper-modal" class="modal fade" tabindex="-1" role="dialog" data-backdrop="false">',
     '<div class="modal-dialog" role="document">',
     '<div class="modal-content">',
     '<div class="modal-header">',
@@ -92,7 +92,7 @@ var ImageCropper = {
     '<h4 class="modal-title">{{t "region-link"}}</h4>',
     '</div>',
     '<div class="modal-body">',
-    '<input id="image-url" type="text">',
+    '<input id="image-url" type="text" value="{{imageUrl}}">',
     '<button type="button" class="btn btn-default" id="copy-to-clipboard" title="{{t "copy-to-clipboard"}}">',
     '<i class="fa fa-clipboard" aria-hidden="true"></i>',
     '</button>',
@@ -100,35 +100,39 @@ var ImageCropper = {
     '<h4 class="block-title">{{t "options"}}</h4>',
     '<div class="option-type size">{{t "size"}}:</div>',
     '<input id="size-selector" type="range" min="1" name="size" value="100">',
-    '<span id="size-label">{{t "selected-size"}}:<span id="relative">100%</span>({{t "approx"}}<span id="absolute"></span>{{t "pixel"}})</span>',
+    '<span id="size-label">{{t "selected-size"}}:<span id="relative">100%</span>',
+    '({{t "approx"}}<span id="absolute">{{imageUrlParams.region.w}}x{{imageUrlParams.region.h}}</span>{{t "pixel"}})</span>',
     '<div class="option-type rotation">{{t "rotation"}}:</div>',
     '<label class="radio-inline"><input type="radio" name="rotation" value="0" checked>0°</label>',
     '<label class="radio-inline"><input type="radio" name="rotation" value="90">90°</label>',
     '<label class="radio-inline"><input type="radio" name="rotation" value="180">180°</label>',
     '<label class="radio-inline"><input type="radio" name="rotation" value="270">270°</label>',
     '<div class="option-type quality">{{t "quality"}}:</div>',
-    '<label class="radio-inline"><input type="radio" name="quality" value="default" checked>{{t "default"}}</label>',
+    '<label class="radio-inline"><input type="radio" name="quality" value="{{imageUrlParams.quality}}" checked>{{t "default"}}</label>',
     '<label class="radio-inline"><input type="radio" name="quality" value="color">{{t "color"}}</label>',
     '<label class="radio-inline"><input type="radio" name="quality" value="gray">{{t "gray"}}</label>',
     '<label class="radio-inline"><input type="radio" name="quality" value="bitonal">{{t "bitonal"}}</label>',
     '<hr>',
     '</div>',
     '<div id="preview">',
-    '<h4 class="block-title">{{t "preview"}} <i class="fa fa-spinner" aria-hidden="true"></i></h4>',
-    '<h5>{{t "preview-image-link"}}</h5>',
-    '<img id="preview-image" alt="{{t "preview-image-error"}}">',
+    '<h4 class="block-title">{{t "preview"}} <i class="fa fa-spinner fa-spin" aria-hidden="true"></i></h4>',
+    '<h5><a href="{{imageUrl}}" id="preview-image-link" target="_blank">{{t "preview-image-link"}}</a></h5>',
+    '<img id="preview-image" alt="{{t "preview-image-error"}}" src="{{imageUrl}}">',
     '</div>',
+    '{{#if licenseUrl}}',
     '<div id="license-message" class="alert alert-warning" role="alert">',
     '{{t "license-message"}}:',
-    '<a href="#" id="license-link" target="_blank"></a>',
+    '<a href="{{licenseUrl}}" id="license-link" target="_blank">{{licenseUrl}}</a>',
     '</div>',
+    '{{/if}}',
     '</div>',
     '<div class="modal-footer">',
     '<button type="button" class="btn btn-default" data-dismiss="modal">{{t "close"}}</button>',
     '</div>',
     '</div>',
     '</div>',
-    '</div>'
+    '</div>',
+    '<div class="modal-backdrop fade in"></div>'
   ].join('')),
 
   /* adds the locales to the internationalization module of the viewer */
@@ -152,16 +156,16 @@ var ImageCropper = {
       $('#image-cropper-modal #image-url').select();
       document.execCommand('copy');
     });
-    $(document.body).on('change', 'input[name="size"], input[name="rotation"], input[name="quality"]', function(event){
-      var buttonName = $(event.target).attr('name');
-      var scaleFactor = $(event.target).closest('#options').find('#size-selector').val();
+    $(document.body).on('change', 'input[name="size"], input[name="rotation"], input[name="quality"]', function(evt){
+      var buttonName = $(evt.target).attr('name');
+      var scaleFactor = $(evt.target).closest('#options').find('#size-selector').val();
       if(buttonName === 'size'){
         this.imageUrlParams.size = 'pct:' + scaleFactor;
         $('#image-cropper-modal #size-label > #relative').text(scaleFactor + '%');
       }else if(buttonName === 'rotation'){
-        this.imageUrlParams.rotation = $(event.target).val();
+        this.imageUrlParams.rotation = $(evt.target).val();
       }else if(buttonName === 'quality'){
-        this.imageUrlParams.quality = $(event.target).val();
+        this.imageUrlParams.quality = $(evt.target).val();
       }
       if(['size', 'rotation'].indexOf(buttonName) !== -1){
         var absoluteWidth = Math.ceil(scaleFactor / 100 * this.imageUrlParams.region.w);
@@ -263,10 +267,10 @@ var ImageCropper = {
   },
 
   /* calculates the positions of the given element and the cursor */
-  calculatePositions: function(element, event){
+  calculatePositions: function(element, evt){
     return {
       'element': element.offset(),
-      'mouse': { 'top': event.pageY, 'left': event.pageX }
+      'mouse': { 'top': evt.pageY, 'left': evt.pageX }
     };
   },
 
@@ -354,12 +358,36 @@ var ImageCropper = {
     });
   },
 
+  /* generates the image url to the given params */
+  generateImageUrl: function(viewerWindow, eventTarget){
+    var currentImage = viewerWindow.imagesList[viewerWindow.currentImgIndex];
+    var service = currentImage.images[0].resource.service || currentImage.images[0].resource.default.service;
+    var currentOverlayDimensions = $.extend(
+      $(eventTarget).closest('.cropping-overlay').position(), {
+        'height': $(eventTarget).closest('.cropping-overlay').height(),
+        'width': $(eventTarget).closest('.cropping-overlay').width()
+      }
+    );
+    var currentTiledImage = viewerWindow.canvases[viewerWindow.canvasID].getVisibleImages()[0].osdTiledImage;
+    this.imageUrlParams = {
+      'imageBaseUrl': Mirador.Iiif.getImageUrl(currentImage),
+      'region': this.calculateImageCoordinates(
+        currentOverlayDimensions, currentTiledImage, true, true, viewerWindow.windowId
+      ),
+      'size': 'full',
+      'rotation': 0,
+      'quality': Mirador.Iiif.getVersionFromContext(service['@context']) === '2.0' ? 'default' : 'native'
+    };
+    return this.imageUrlTemplate(this.imageUrlParams);
+  },
+
   /* extracts the license link from the manifest entry */
-  getLicenseInformation: function(license){
-    if(typeof license !== 'undefined'){
-      return $(Mirador.MetadataView.prototype.addLinksToUris(license)).attr('href');
+  getLicenseInformation: function(viewerWindow){
+    var license = viewerWindow.manifest.jsonLd.license;
+    if(!this.options.showLicense || typeof license === 'undefined'){
+      return null;
     }
-    return false;
+    return $(Mirador.MetadataView.prototype.addLinksToUris(license)).attr('href');
   },
 
   /* initializes the plugin */
@@ -371,9 +399,9 @@ var ImageCropper = {
     this.injectButtonToCanvasControls();
     this.injectDraggingEventHandlers();
     this.injectResizingEventHandlers();
+    this.injectViewerEventHandler();
     this.injectWindowEventHandler();
     this.injectOverlayToCanvas();
-    this.injectModalToDom();
   },
 
   /* injects the button to the canvas controls */
@@ -405,24 +433,24 @@ var ImageCropper = {
     var origFunc = Mirador.ImageView.prototype.listenForActions;
     Mirador.ImageView.prototype.listenForActions = function(){
       origFunc.apply(this);
-      this.element.on('mousedown', '.cropping-overlay > .resize-frame', function(event){
-        if(event.which === 1){
+      this.element.on('mousedown', '.cropping-overlay > .resize-frame', function(evt){
+        if(evt.which === 1){
           this_.dragging = true;
-          currentPositions = this_.calculatePositions($(this).parent(), event);
+          currentPositions = this_.calculatePositions($(this).parent(), evt);
           offsets.canvas = $(this).closest('.mirador-osd').offset();
           offsets.mouse = {
             'x': currentPositions.mouse.left - currentPositions.element.left,
             'y': currentPositions.mouse.top - currentPositions.element.top
           };
         }
-      }).on('mousemove', '.mirador-osd', function(event){
+      }).on('mousemove', '.mirador-osd', function(evt){
         if(this_.dragging){
-          event.preventDefault();
-          currentPositions = this_.calculatePositions(this.croppingOverlay, event);
+          evt.preventDefault();
+          currentPositions = this_.calculatePositions(this.croppingOverlay, evt);
           var currentTiledImage = this.canvases[this.canvasID].getVisibleImages()[0].osdTiledImage;
           this_.changeOverlayPosition(
             currentPositions, offsets, this.croppingOverlay,
-            event.currentTarget, currentTiledImage, this.windowId
+            evt.currentTarget, currentTiledImage, this.windowId
           );
         }
       }.bind(this)).on('mouseup', '.cropping-overlay > .resize-frame', function(){
@@ -432,22 +460,44 @@ var ImageCropper = {
   },
 
   /* injects the modal to the dom */
-  injectModalToDom: function(){
-    var this_ = this;
-    var origFunc = Mirador.Viewer.prototype.setupViewer;
-    Mirador.Viewer.prototype.setupViewer = function(){
-      origFunc.apply(this);
-      var options = this.state.getStateProperty('imageCropper');
-      if($.isPlainObject(options)){
-        this_.options = options;
-      }
-      document.body.insertAdjacentHTML('beforeend', this_.modalTemplate());
+  injectModalToViewerWindow: function(viewerWindow, eventTarget){
+    var imageUrl = this.generateImageUrl(viewerWindow, eventTarget);
+    var licenseUrl = this.getLicenseInformation(viewerWindow);
+    viewerWindow.element[0].insertAdjacentHTML('beforeend', this.modalTemplate({
+      'imageUrl': imageUrl,
+      'imageUrlParams': this.imageUrlParams,
+      'licenseUrl': licenseUrl
+    }));
+    $('#image-cropper-modal').on('show.bs.modal', function(){
       if(window.ShareButtons !== undefined){
-        ShareButtons.init(this_.options.showShareButtonsInfo);
+        this.setManifestData(viewerWindow.manifest.jsonLd);
         ShareButtons.injectButtonsToDom('#image-cropper-modal .modal-footer', 'afterbegin');
+        ShareButtons.updateButtonLinks({
+          'attribution': this.manifestAttribution,
+          'label': this.manifestLabel,
+          'link': imageUrl,
+          'thumbnailUrl': this.imageUrlTemplate(
+            $.extend({}, this.imageUrlParams, {'size': '280,'})
+          )
+        });
       }
-    };
-    this.addModalEventHandlers();
+    }.bind(this));
+    $('#image-cropper-modal').on('shown.bs.modal', function(){
+      $('#image-cropper-modal #image-url').select();
+    });
+    $('#image-cropper-modal').on('hidden.bs.modal', function(){
+      $(this).siblings('.modal-backdrop').remove();
+      $(this).remove();
+    });
+    $('#image-cropper-modal').on('click', function(e){
+      if(e.target === this){
+        $(this).modal('hide');
+      }
+    });
+    $('#image-cropper-modal #preview-image').on('error load', function(){
+      $('#image-cropper-modal .fa-spinner').hide().removeClass('fa-spin');
+    });
+    $('#image-cropper-modal').modal('show');
   },
 
   /* injects the cropping overlay to the canvas */
@@ -482,17 +532,17 @@ var ImageCropper = {
     var origFunc = Mirador.ImageView.prototype.listenForActions;
     Mirador.ImageView.prototype.listenForActions = function(){
       origFunc.apply(this);
-      this.element.on('mousedown', '.resize-anchor, .resize-bar', function(event){
-        if(event.which === 1){
-          typeOfResizeElement = event.target.className.split(' ').pop();
+      this.element.on('mousedown', '.resize-anchor, .resize-bar', function(evt){
+        if(evt.which === 1){
+          typeOfResizeElement = evt.target.className.split(' ').pop();
           this_.resizing = true;
           offsets.canvas = $(this).closest('.mirador-osd').offset();
           offsets.element = $(this).parent().offset();
         }
-      }).on('mousemove', '.mirador-osd', function(event){
+      }).on('mousemove', '.mirador-osd', function(evt){
         if(this_.resizing){
-          event.preventDefault();
-          currentMousePosition = this_.calculatePositions(this.croppingOverlay, event).mouse;
+          evt.preventDefault();
+          currentMousePosition = this_.calculatePositions(this.croppingOverlay, evt).mouse;
           var currentTiledImage = this.canvases[this.canvasID].getVisibleImages()[0].osdTiledImage;
           this_.changeOverlayDimensions(
             typeOfResizeElement, currentMousePosition, offsets,
@@ -511,75 +561,27 @@ var ImageCropper = {
     var origFunc = Mirador.ImageView.prototype.bindEvents;
     Mirador.ImageView.prototype.bindEvents = function(){
       origFunc.apply(this);
-      this.element.on('click', '.cropping-overlay > .share-button', function(event){
-        var currentImage = this.imagesList[this.currentImgIndex];
-        var service = currentImage.images[0].resource.service || currentImage.images[0].resource.default.service;
-        var currentOverlayDimensions = $.extend(
-          $(event.target).closest('.cropping-overlay').position(), {
-            'height': $(event.target).closest('.cropping-overlay').height(),
-            'width': $(event.target).closest('.cropping-overlay').width()
-        });
-        var currentTiledImage = this.canvases[this.canvasID].getVisibleImages()[0].osdTiledImage;
-        var license = false;
-        if(this_.options.showLicense){
-          license = this_.getLicenseInformation(this.manifest.jsonLd.license);
-        }
-        this_.imageUrlParams = {
-          'imageBaseUrl': Mirador.Iiif.getImageUrl(currentImage),
-          'region': this_.calculateImageCoordinates(
-            currentOverlayDimensions, currentTiledImage, true, true, this.windowId
-          ),
-          'size': 'full',
-          'rotation': 0,
-          'quality': Mirador.Iiif.getVersionFromContext(service['@context']) === '2.0' ? 'default' : 'native'
-        };
-        var imageUrl = this_.imageUrlTemplate(this_.imageUrlParams);
-        $('#image-cropper-modal #image-url').val(imageUrl);
-        $('#image-cropper-modal #preview-image-link').attr('href', imageUrl);
-        $('#image-cropper-modal .fa-spinner').addClass('fa-spin').show();
-        $('#image-cropper-modal #preview-image').attr('src', imageUrl).on('error load', function(){
-          $('#image-cropper-modal .fa-spinner').hide().removeClass('fa-spin');
-        });
-        $($('#image-cropper-modal').find('.quality + label > input')).val(
-          this_.imageUrlParams.quality
-        );
-        $($('#image-cropper-modal').find('.quality + label > span')).text(
-          this_.imageUrlParams.quality
-        );
-        $('#image-cropper-modal').modal('show');
-        $('#image-cropper-modal').on('shown.bs.modal', function(){
-          $('#image-cropper-modal #image-url').select();
-          $('#image-cropper-modal #size-label > #absolute').text(
-            this_.imageUrlParams.region.w + 'x' + this_.imageUrlParams.region.h
-          );
-        });
-        $('#image-cropper-modal').on('hidden.bs.modal', function(){
-          $($('#image-cropper-modal').find('.option-type + label > input')).prop(
-            'checked', true
-          );
-          $('#image-cropper-modal .option-type + input[name="size"]').val(100);
-          $('#image-cropper-modal #size-label > #relative').text('100%');
-        });
-        if(license){
-          $('#license-link').attr('href', license).text(license);
-          $('#license-message').show();
-        }else{
-          $('#license-message').hide();
-          $('#license-link').attr('href', '#').text('');
-        }
-        if(window.ShareButtons !== undefined){
-          this_.setManifestData(this.manifest.jsonLd);
-          ShareButtons.updateButtonLinks({
-            'attribution': this_.manifestAttribution,
-            'label': this_.manifestLabel,
-            'link': imageUrl,
-            'thumbnailUrl': this_.imageUrlTemplate(
-              $.extend({}, this_.imageUrlParams, {'size': '280,'})
-            )
-          });
-        }
+      this.element.on('click', '.cropping-overlay > .share-button', function(evt){
+        this_.injectModalToViewerWindow(this, evt.target);
       }.bind(this));
     };
+  },
+
+  /* injects the needed viewer event handler */
+  injectViewerEventHandler: function(){
+    var this_ = this;
+    var origFunc = Mirador.Viewer.prototype.setupViewer;
+    Mirador.Viewer.prototype.setupViewer = function(){
+      origFunc.apply(this);
+      var options = this.state.getStateProperty('imageCropper');
+      if($.isPlainObject(options)){
+        this_.options = options;
+      }
+      if(window.ShareButtons !== undefined){
+        ShareButtons.init(this_.options.showShareButtonsInfo);
+      }
+    };
+    this.addModalEventHandlers();
   },
 
   /* injects an event handler for saving the image dimensions in the windows */
@@ -588,11 +590,11 @@ var ImageCropper = {
     var origFunc = Mirador.Window.prototype.listenForActions;
     Mirador.Window.prototype.listenForActions = function(){
       origFunc.apply(this);
-      this.eventEmitter.subscribe('windowUpdated', function(event, data){
-        if (this.id !== data.id || !data.viewType) {
+      this.eventEmitter.subscribe('windowUpdated', function(evt, data){
+        if(this.id !== data.id || !data.viewType){
           return;
         }
-        if (data.viewType === 'ImageView') {
+        if(data.viewType === 'ImageView'){
           this_.imageDimensions[this.id] = {
             'height': this.canvases[data.canvasID].canvas.height,
             'width': this.canvases[data.canvasID].canvas.width
